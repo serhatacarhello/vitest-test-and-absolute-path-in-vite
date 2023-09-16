19399776b27314b2f54d1e3043e5979c2a7fcd0e
serhatacarhello <serhat.acar.tr@gmail.com>
2023-09-16 22:25:32 +0300

    counter test added

--- tests/App.test.jsx
+++ tests/App.test.jsx
@@ -1,10 +1,25 @@
 import { render, screen } from '@testing-library/react'
 import App from '../src/App'
+import { describe, expect, it } from 'vitest'
+import userEvent from '@testing-library/user-event'
 
 describe('App', () => {
   it('renders headline', () => {
     render(<App />)
     const headline = screen.getByText(/Vite React/i)
     expect(headline).toBeInTheDocument()
+  })
+
+  it('renders counter', async () => {
+    render(<App />)
+    const user = userEvent.setup()
+    const counterBtn = screen.getByRole('button')
+    await user.click(counterBtn)
+
+    // Expect counter to increment to '1'
+    expect(counterBtn).toHaveTextContent('1')
+
+    await user.dblClick(counterBtn)
+    expect(counterBtn).toHaveTextContent('3')
   })
 })
